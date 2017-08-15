b505ed4efc3c2ad17e3da2e110eb28edc4271259
Lionel Villard <villard@us.ibm.com>
2017-08-15 14:21:16 -0400

    install dependencies in .openwhisk

--- bin/install.sh
+++ bin/install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 DIRNAME="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd ${DIRNAME}/..
-npm install --production
-npm link 
-
-cd ${DIRNAME}
+mkdir -p ~/.openwhisk
+cd ~/.openwhisk
+rm -rf whisk-libs
 git clone git@github.ibm.com:villard/whisk-libs.git --depth 2
 cd whisk-libs
 npm install --production
 npm link
 
 cd ${DIRNAME}/..
+npm install --production
+npm link 
 npm link @openwhisk-libs/wsk
