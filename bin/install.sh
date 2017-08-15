06c7e0d4122c31c729e8192614b8e76f77233adf
Lionel Villard <villard@us.ibm.com>
2017-08-15 16:38:59 -0400

    pin wsk version

--- bin/install.sh
+++ bin/install.sh
@@ -4,8 +4,9 @@ DIRNAME="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p ~/.openwhisk
 cd ~/.openwhisk
 rm -rf whisk-libs
-git clone git@github.ibm.com:villard/whisk-libs.git --depth 2
+git clone git@github.ibm.com:villard/whisk-libs.git 
 cd whisk-libs
+git checkout tags/v0.1.0
 npm install --production
 npm link
 
--- wskp-update.js
+++ wskp-update.js
@@ -4,14 +4,14 @@ const { exec } = require('child-process-promise')
 const  pkg = require('./package.json');
 
 const update = async () => {
-    console.log('Updating...')
-    const version = await exec('npm show wskp version')
-    if (version && version !== pkg.version) {
-        console.log(chalk.yellow('A new version of this CLI is available. Updating...'))
-        await exec('npm update wskp -g')
-    } else {
-        console.log('wskp is up to date')
-    }
+    //console.log('Updating...')
+    // const version = await exec('npm show wskp version')
+    // if (version && version !== pkg.version) {
+    //     console.log(chalk.yellow('A new version of this CLI is available. Updating...'))
+    //     await exec('npm update wskp -g')
+    // } else {
+         console.log('wskp is up to date')
+    // }
 }
 
 update()
