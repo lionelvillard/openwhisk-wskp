2a042ab76d5129d7f187291424722a8bbac98887
Lionel Villard <villard@us.ibm.com>
2017-08-16 09:09:25 -0400

    another attempts this time using ssh keys

--- bin/install.sh
+++ bin/install.sh
@@ -4,7 +4,7 @@ DIRNAME="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p ~/.openwhisk
 cd ~/.openwhisk
 rm -rf whisk-libs
-git clone https://github.ibm.com/villard/whisk-libs.git
+git clone git@github.ibm.com:villard/whisk-libs.git 
 cd whisk-libs
 git checkout tags/v0.1.0
 npm install --production
