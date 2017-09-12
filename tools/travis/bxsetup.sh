208d1558201b613acd11e5cd4cabb3176dcc0dc6
Lionel Villard <villard@us.ibm.com>
2017-09-11 20:37:26 -0400

    use $IBMCLOUD_SPACE instead of IBMCLOUD_TARGET

--- tools/travis/bxsetup.sh
+++ tools/travis/bxsetup.sh
@@ -7,7 +7,7 @@ sudo ./install_bluemix_cli
 
 bx plugin install Cloud-Functions -r Bluemix
 
-bx login -a https://api.ng.bluemix.net -o $IBMCLOUD_ORG -s $IBMCLOUD_TARGET
+bx login -a https://api.ng.bluemix.net -o $IBMCLOUD_ORG -s $IBMCLOUD_SPACE
 
 # generate .wskprops
 bx wsk property get
