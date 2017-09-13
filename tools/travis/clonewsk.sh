cd6601af77d7ac30bf0b75d67091b59fba2ed521
Lionel Villard <villard@us.ibm.com>
2017-09-13 14:36:49 -0400

    fix typos

--- tools/travis/clonewsk.sh
+++ tools/travis/clonewsk.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ "$LATEST_WKD" == true ]; then
+if [ "$LATEST_WSKD" = true ]; then
   git clone https://github.com/lionelvillard/openwhisk-deploy.git --depth 3
   cd openwhisk-deploy
   npm i 
