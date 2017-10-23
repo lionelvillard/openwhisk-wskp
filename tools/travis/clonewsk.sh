66041a416fb916328fe3cbc9893bd0b6d58030ae
Lionel Villard <villard@us.ibm.com>
2017-10-23 16:35:14 -0400

    fix travis configuration

--- tools/travis/clonewsk.sh
+++ tools/travis/clonewsk.sh
@@ -3,8 +3,8 @@
 set -e
 
 if [ "$LATEST_WSKD" = true ]; then
-  git clone https://github.com/lionelvillard/openwhisk-deploy.git --depth 3
-  cd openwhisk-deploy
+  git clone https://github.com/lionelvillard/openwhisk-project.git --depth 3
+  cd openwhisk-project
   npm i 
   npm run compilenowatch
   npm link
